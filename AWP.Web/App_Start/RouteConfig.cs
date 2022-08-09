3d83a7ca56429af9283f4b69c705f461fde0023e
ychung-mot <young-jin.chung@dxcas.com>
2022-08-09 16:06:47 -0700

    default to index

--- AWP.Web/App_Start/RouteConfig.cs
+++ AWP.Web/App_Start/RouteConfig.cs
@@ -10,11 +10,15 @@ namespace AWP.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "DefaultToIndex",
+                url: "{_}",
+                defaults: new { controller = "Home", action = "Index", id = "" }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = "" }
             );
-
         }
     }
 }
