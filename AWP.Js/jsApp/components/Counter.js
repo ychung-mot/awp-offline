15c9f9716678a3b63864582f56a2e0274280e8b5
Darrel Siegle <darrel.siegle@dxcas.com>
2022-08-11 16:35:39 -0700

    changed counter page to geolocate

--- AWP.Js/jsApp/components/Counter.js
+++ AWP.Js/jsApp/components/Counter.js
@@ -1,31 +1,53 @@
-import React, { Component } from 'react';
+const getPosition = () => {
+	return new Promise((res, rej) => {
+		navigator.geolocation.getCurrentPosition(res, rej, {
+			enableHighAccuracy: true,
+			timeout: 5000,
+			maximumAge: 0,
+		});
+	});
+};
 
-export class Counter extends Component {
-  static displayName = Counter.name;
+class SimpleExample extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { locationData: { coords: { latitude: null, longitude: null, accuracy: 0, speed: 0 } } };
+		this.getPosition = this.getPosition.bind(this);
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
+	getPosition() {
+		getPosition().then((res) => {
+			console.log(res);
+			this.setState({ locationData: res });
+		});
+	}
 
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
+	render() {
+		return (
+			<div>
+				<h1>Get GeoLocation</h1>
 
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
+				<p>This is a simple example of a React component that pulls current GeoLocation</p>
 
-        <p>This is a simple example of a React component.</p>
-
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
-    );
-  }
+				<p aria-live="polite">
+					Longtitude: <strong>{this.state.locationData.coords.longitude}</strong>
+				</p>
+				<p aria-live="polite">
+					Latitude: <strong>{this.state.locationData.coords.latitude}</strong>
+				</p>
+				<p aria-live="polite">
+					Accuracy: <strong>{this.state.locationData.coords.accuracy}m</strong>
+				</p>
+				<p aria-live="polite">
+					Speed:{' '}
+					<strong>{this.state.locationData.coords.speed == null ? 0 : this.state.locationData.coords.speed}m/s</strong>
+				</p>
+				<button className="btn btn-primary" onClick={this.getPosition}>
+					Get Position
+				</button>
+			</div>
+		);
+	}
 }
+
+ReactDOM.render(<SimpleExample />, document.getElementById('example'));
